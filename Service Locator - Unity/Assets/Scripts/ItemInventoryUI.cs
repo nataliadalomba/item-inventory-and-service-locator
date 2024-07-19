2e53baea3b320e2e54f396991fa7de21c550b136
Natalia DaLomba <nataliadalomba@proton.me>
2024-07-18 21:35:02 -0500

    Updated ItemInventory script

--- Service Locator - Unity/Assets/Scripts/ItemInventoryUI.cs
+++ Service Locator - Unity/Assets/Scripts/ItemInventoryUI.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 using TMPro;
 
 [System.Serializable]
-public class ItemInventory : MonoBehaviour {
+public class ItemInventoryUI : MonoBehaviour {
     public Item[] slots;
     public GameObject slotPrefab;
     public Transform gridParent; //reference to GridLayoutGroup's transform
